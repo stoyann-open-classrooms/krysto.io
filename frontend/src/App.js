6d02f10e79ed5c5a7da70cf2816955f7830dd1ae
Stoyann Velten <stoyann.openclassrooms@gmail.com>
2024-11-03 14:39:58 +1100

    push test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', textAlign: 'center' }}>
-      <h1>Welcome to Krysto IO</h1>
+      <h1>Welcome to Krysto IO test</h1>
       <p style={{ fontSize: '1.5em', color: '#555' }}>Site en construction 🚧</p>
     </div>
   );
