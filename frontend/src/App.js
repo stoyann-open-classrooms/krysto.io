b8a78138ef24ca5639d152a1462ed2f327a7c348
Stoyann Velten <stoyann.openclassrooms@gmail.com>
2024-11-03 14:49:46 +1100

    install tailwind and redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', textAlign: 'center' }}>
+    <div className='bg-blue-200' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', textAlign: 'center' }}>
       <h1>Welcome to Krysto IO test</h1>
       <p style={{ fontSize: '1.5em', color: '#555' }}>Site en construction 🚧</p>
     </div>
