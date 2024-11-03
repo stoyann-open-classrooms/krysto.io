fa56513a113a4be58e65c08b58288d6b99be1fce
Stoyann Velten <stoyann.openclassrooms@gmail.com>
2024-11-03 16:29:17 +1100

    secure for send mail in prod

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -5,6 +5,7 @@ const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
+    secure: true,
     auth: {
       user: process.env.SMTP_EMAIL,
       pass: process.env.SMTP_PASSWORD,
